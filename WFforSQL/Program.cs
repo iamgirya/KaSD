ef06a488aa01275f843fa441323519d0ca747dd1
iamgirya <iamgirya@yandex.ru>
2022-05-02 13:11:12 +0300

    Создание проекта

---
+++ WFforSQL/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace WFforSQL
+{
+    static class Program
+    {
+        public static main MainForm;
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            MainForm = new main();
+            Application.Run(MainForm);
+        }
+    }
+}
